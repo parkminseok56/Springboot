437b508052ecca0dc7c81ed2cf8458f4fb2b8d5f
parkminseok56 <133790403+parkminseok56@users.noreply.github.com>
2023-06-12 16:57:22 +0900

    Initial commit

---
+++ SpringBoot_G01/src/test/java/com/ezen/g01/SpringBootG01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezen.g01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootG01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
