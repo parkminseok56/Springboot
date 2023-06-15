9a227f65ee3515dabb094fcde20694348c954876
parkminseok56 <133790403+parkminseok56@users.noreply.github.com>
2023-06-15 18:00:56 +0900

    2023-06-15 9 교시

--- SpringBoot_G14/src/main/java/com/ezen/g14/controller/BoardController.java
+++ SpringBoot_G14/src/main/java/com/ezen/g14/controller/BoardController.java
@@ -3,6 +3,7 @@ package com.ezen.g14.controller;
 
 
 import java.util.HashMap;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -12,6 +13,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.ezen.g14.dto.BoardVO;
+import com.ezen.g14.dto.Paging;
 import com.ezen.g14.service.BoardService;
 
 @Controller
@@ -41,13 +44,16 @@ public class BoardController {
 			Paging paging = new Paging();
 			// ... Paging 까지 controller 에서 처리하기엔 내용이 복잡해짐
 			// request에 담겨 있는 page 파라미터를 service의 getBoardList()메서드에 보내서 해당 게시물 리스트를 리턴
-*/				
+         */				
 			
 			// page 파라미터를 품고 있는 request service에 보내서 페이지 처리 및 해당 게시글 조회 후
 			// HashMap에 모두 담아 리턴받을 예정임
 			HashMap <String, Object> result = bs.getBoardList( request);
-			
-			
+			// List<BoardVO> list = ( List<BoardVO> )result.get("boardList");
+			// Paging paging = (Paging) result.get("paging");
+			mav.addObject("boardList", ( List<BoardVO>) result.get("boardList"));
+			mav.addObject("paging", ( Paging) result.get("paging"));
+			mav.setViewName("board/main");
 		}
 		return mav;
 		} 
